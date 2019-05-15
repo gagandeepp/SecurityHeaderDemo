d0894d48026ba40549ae8c11a523fc9a1932dc45
Gagan Deep <gagandeepprathar@outlook.com>
2019-05-15 12:36:38 +0530

    Initial Commit

---
+++ OwaspSecurityHeaderDemo/Middleware/OwaspSecurityHeaderMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace OwaspSecurityHeaderDemo.Middlewares
+{
+    public class OwaspSecurityHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public OwaspSecurityHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// The main task of the middleware. This will be invoked whenever
+        /// the middleware fires
+        /// </summary>
+        /// <param name="httpContext">The <see cref="HttpContext" /> for the current request or response</param>
+        /// <returns></returns>
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //CSP Header
+            httpContext.Response.Headers.Add("Content-Security-Policy",
+                              "script-src 'self'; " +
+                "style-src 'self'; " +
+                "img-src 'self'");
+            //XContent Type Header
+            httpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            //X frame header
+            httpContext.Response.Headers.Add("X-FRAME-Options", "SAMEORIGIN"); // Various options can be set like DENY,ALLOW-FROM
+            //Xss header
+            httpContext.Response.Headers.Add("X-Xss-Protection", "1"); // 0:disable,1:Protection,mode=block,report=url
+            //Cache Header
+            httpContext.Response.Headers.Add("Cache-Control", "public");
+            // Call the next middleware in the chain
+            await _next.Invoke(httpContext);
+        }
+    }
+}
---
+++ OwaspSecurityHeaderDemo/Startup.cs
@@ -0,0 +1,72 @@
+﻿#region snippet_all
+// Unused usings removed
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OwaspSecurityHeaderDemo.Models;
+using System;
+
+namespace OwaspSecurityHeaderDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the 
+        //container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
+            services.AddResponseCaching();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+                //options.ExcludedHosts.Add("example.com");
+                //options.ExcludedHosts.Add("www.example.com");
+            });
+
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = 5001;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP 
+        //request pipeline.
+        #region snippet_configure
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+                app.UseHttpsRedirection();
+            }
+            app.UseMiddleware<Middlewares.OwaspSecurityHeaderMiddleware>();
+            app.UseResponseCaching();
+            app.UseAuthentication();
+            app.UseMvc();
+            
+        }
+        #endregion
+    }
+}
+#endregion
